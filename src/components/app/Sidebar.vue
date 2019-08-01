63a15f03bc743d2e9b772c9b6f2e40a7fe15faa9
Your Name <you@example.com>
2019-08-01 17:39:54 +0300

    Добавлен роутинг для страниц

---
+++ src/components/app/Sidebar.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul class="sidenav app-sidenav open">
+    <router-link
+      v-for="link in links"
+      :key="link.url"
+      tag="li"
+      active-class="active"
+      :to="link.url"
+      :exact="link.exact"
+    >
+      <a class="waves-effect waves-orange pointer" href="#">{{link.title}}</a>
+    </router-link>
+  </ul>
+</template>
+
+<script>
+  export default {
+    name: 'Sidebar',
+    data: () => ({
+      links:[
+        {title: 'Счет', url: '/', exact:true},
+        {title: 'История', url: '/history'},
+        {title: 'Планирование', url: '/planning'},
+        {title: 'Новая запись', url: '/record'},
+        {title: 'Категории', url: '/categories'},
+      ]
+    })
+  }
+</script>
+
+<style scoped>
+
+</style>
