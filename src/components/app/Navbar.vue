7f2e6617feef241b83f0ccc4e491ef30984bd9b5
Your Name <you@example.com>
2019-08-05 17:08:20 +0300

    Добавлена логика в дропдаун

--- src/components/app/Navbar.vue
+++ src/components/app/Navbar.vue
@@ -14,6 +14,7 @@
             class="dropdown-trigger black-text"
             href="#"
             data-target="dropdown"
+            ref="dropdown"
           >
             USER NAME
             <i class="material-icons right">arrow_drop_down</i>
@@ -21,13 +22,13 @@
           
           <ul id='dropdown' class='dropdown-content'>
             <li>
-              <a href="#" class="black-text">
+              <router-link to="/profile" class="black-text">
                 <i class="material-icons">account_circle</i>Профиль
-              </a>
+              </router-link>
             </li>
             <li class="divider" tabindex="-1"></li>
             <li>
-              <a href="#" class="black-text">
+              <a href="#" class="black-text" @click.prevent="logout">
                 <i class="material-icons">assignment_return</i>Выйти
               </a>
             </li>
@@ -40,7 +41,18 @@
 
 <script>
   export default {
-    name: 'Navbar'
+    name: 'Navbar',
+    methods:{
+      logout(){
+        console.log('logout');
+        this.$router.push('/login?message=logout')
+      }
+    },
+    mounted () {
+      M.Dropdown.init(this.$refs.dropdown,{
+        constrainWidth: false
+      })
+    }
   }
 </script>
 
