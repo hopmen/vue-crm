a854b7a153c4ede0c00eed1ab55dc88dc9da06b8
Your Name <you@example.com>
2019-08-07 17:24:32 +0300

    Добавлено аутентификация через firebase

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import auth from './auth'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
@@ -12,5 +12,8 @@ export default new Vuex.Store({
   },
   actions: {
 
+  },
+  modules:{
+    auth
   }
 })
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -80,7 +80,7 @@
       }
     },
     methods: {
-      submitHandler () {
+      async submitHandler () {
         if (this.$v.$invalid) {
           this.$v.$touch()
           return
@@ -89,9 +89,10 @@
           email: this.email,
           password: this.password
         }
-
-        console.log(formData)
-        this.$router.push('/')
+        try {
+          await this.$store.dispatch('login', formData)
+          this.$router.push('/')
+        }catch (e) {}
       }
     }
   }
