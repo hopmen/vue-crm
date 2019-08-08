c4d604b24926bbacce60a72a74bdc12c331e14bf
Your Name <you@example.com>
2019-08-08 17:23:02 +0300

    Добавлено регистрация нового пользователя

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -97,7 +97,7 @@
       agree: { checked: v => v }
     },
     methods: {
-      submitHandler () {
+      async submitHandler () {
         if (this.$v.$invalid) {
           this.$v.$touch()
           return
@@ -107,8 +107,10 @@
           password: this.password,
           name: this.name
         }
-        console.log(formData);
-        this.$router.push('/')
+        try {
+          await this.$store.dispatch(`register`, formData)
+          this.$router.push('/')
+        }catch (e) {}
       }
     }
   }
