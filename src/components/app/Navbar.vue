0979047307abd229f0c8cd6b55de5bb2f863e06c
Your Name <you@example.com>
2019-10-15 12:47:33 +0300

    second

---
+++ src/components/app/Navbar.vue
@@ -0,0 +1,40 @@
+<template>
+  <nav class="navbar orange lighten-1">
+    <div class="nav-wrapper">
+      <div class="navbar-left">
+        <a href="#" @click.prevent="$emit('click')">
+          <i class="material-icons black-text">dehaze</i>
+        </a>
+        <span class="black-text">12.12.12</span>
+      </div>
+      
+      <ul class="right hide-on-small-and-down">
+        <li>
+          <a
+            class="dropdown-trigger black-text"
+            data-target="dropdown"
+            href="#"
+          >
+            USER NAME
+            <i class="material-icons right">arrow_drop_down</i>
+          </a>
+          
+          <ul class='dropdown-content' id='dropdown'>
+            <li>
+              <a class="black-text" href="#">
+                <i class="material-icons">account_circle</i>Профиль
+              </a>
+            </li>
+            <li class="divider" tabindex="-1"></li>
+            <li>
+              <a class="black-text" href="#">
+                <i class="material-icons">assignment_return</i>Выйти
+              </a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
